e3b61f96f1820f7daab3b9e484aacc3a8e7d0415
Kyle <kj.mckinley@gmail.com>
2021-03-03 21:57:38 -0600

    reformatted character arrays and added needed comments

--- Develop/script.js
+++ Develop/script.js
@@ -3,86 +3,22 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var uppercaseLettersArray = [
-	'A',
-	'B',
-	'C',
-	'D',
-	'E',
-	'F',
-	'G',
-	'H',
-	'I',
-	'J',
-	'K',
-	'L',
-	'M',
-	'N',
-	'O',
-	'P',
-	'Q',
-	'R',
-	'S',
-	'T',
-	'U',
-	'V',
-	'W',
-	'X',
-	'Y',
-	'Z',
+// An array of upper case letters
+var uppercaseLettArray = [
+	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 ];
 
-var lowercaseLettersArray = [
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'n',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
+// An array of lowercase letters
+var lowercaseLettArray = [
+	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 ];
 
+// An array of numbers 1 - 9
 var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var specialCharactersArray = [
-	'!',
-	'#',
-	'$',
-	'%',
-	'&',
-	'(',
-	')',
-	'*',
-	'+',
-	'-',
-	'.',
-	'/',
-	':',
-	';',
-	'<',
-	'=',
-	'>',
-	'?',
-	'@',
+// An array of special characters
+var specialCharArray = [
+	'!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@',
 ];
 
 function generatePassword() {
