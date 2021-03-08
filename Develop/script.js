deaa9e41454592a4502ab76fbb9a3c2bbca4683f
Kyle <kj.mckinley@gmail.com>
2021-03-07 21:02:51 -0600

    added user choices, input and start of if statement for user values.

--- Develop/script.js
+++ Develop/script.js
@@ -26,11 +26,46 @@ var numChar;
 var passNum;
 var passSpecChar;
 
+var passUserChoice;
+
 var generateBtn = document.querySelector("#generate");
 
-// function generatePassword() {
+//click event that generates a password
+generateBtn.addEventListener('click', function() {
+  var pswd = generatePassword();
+  document.getElementById('password').placeholder = pswd;
+});
 
-// };
+// function that generates a new password with certain criteria
+function generatePassword() {
+  // Ask the user for numerical input on the length of the password. Parse to make type an interger.
+  numChar = parseInt(window.prompt("How many Characters Long Would You Like your Password to Be?\nChoose Between 8 and 128 Characters."));
+  
+  //if statment that checks for user validation if not a number, not between 8 and 128, or null
+  if (!numChar) {
+    window.alert("ERROR: You Cannot Leave This Field Blank.\nPlease Enter a Number Between 8 and 128 Characters.");
+  }
+  else if (numChar < 8 || numChar > 128) {
+    numChar = parseInt(window.prompt("ERROR: You Must Enter a Value Between 8 and 128.\nPlease Try Again."));
+  }
+  //else ask user for password criteria.
+  else {
+    passNum = window.confirm("Does This Password Contain Numbers?");
+    passSpecChar = window.confirm("Does This Password Contain Special Characters?");
+    passUpper = window.confirm("Does This Password Contain Upper Case Characters");
+    passLower = window.confirm("Does This Password Contain Lower Case Characters");
+  };
+
+  // Now organize the true and false values of the user input for the password criteria with an if statement
+  // Make flag values for events to triggar if ALL values are false.
+  if (!passNum && passSpecChar && passUpper && passLower) {
+    passUserChoice = window.alert("ERROR: You Must Select at Least 1 of the Promted Critira for Your Password.\nPlease Try Again!");
+  }
+  //if ALL values are true
+  else if (passNum && passSpecChar && passUpper && passLower) {
+    passUserChoice = specialCharArray.concat(numbersArray, uppercaseLettArray, lowercaseLettArray);
+  }
+};
 
 // Write password to the #password input
 function writePassword() {
